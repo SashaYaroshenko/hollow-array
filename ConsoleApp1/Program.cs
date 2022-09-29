c372bf69623cb51daacda805d368daf5eb273cda
Denys <navrotskyidenys@gmail.com>
2022-09-29 22:36:27 +0300

    add wrong program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Kata.IsHollow(new int[] { -1, 0, 0, 0, 3 });
+            Kata.IsHollow(new int[] { 2, 0, 1, 0, 0, 0, 1, 0, 2 });
         }
     }
 
@@ -20,26 +20,30 @@ namespace ConsoleApp1
         {
             int index1 = Array.IndexOf(x, 0);
             int index2 = Array.LastIndexOf(x, 0);
+
             int count1 = 0;
             int count2 = 0;
-            if(index2-index1<3-2)
+            int len = x.Length;
+
+            while (x[index1] != 0 || x[index1 + 1] != 0 || x[index1 + 2] != 0)
             {
-                return false;
-            }
-            else
-            {
-                count1 = index1;
-                count2 = (x.Length-1) - index2;
-                if (count1 == count2)
-                {
-                    return true;
-                }
-                else
+                if (index1 + 2 > index2)
                 {
                     return false;
                 }
+                index1 = Array.IndexOf(x, 0, index1 + 1);
             }
-            
+
+            while (x[index2] != 0 || x[index2 - 1] != 0 || x[index2 - 2] != 0)
+            {
+                if (index1 + 2 > index2)
+                {
+                    return false;
+                }
+                index2 = Array.LastIndexOf(x, 0, index2 - 1);                
+            }
+
+            return true;
         }
     }
 }
